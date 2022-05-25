aeff9aa3ff2e7381846494b3911e38184b0818cc
Jakub Hôrečný <jakub.horecny@st.fri.uniza.sk>
2022-05-25 21:14:26 +0200

    finishing delete:    delete - done    inOrder, preOrder, postOrder, levelOrder tree traversals    starting interval search

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,6 +1,7 @@
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class BinaryTree<T, K extends Comparable<K>> {
     private Node<T, K> root;
@@ -17,10 +18,10 @@ public class BinaryTree<T, K extends Comparable<K>> {
         return (node.getLeftSon() == null && node.getRightSon() == null);
     }
     private boolean isLeftSon(Node<T,K> node) {
-        return (node.getParent().getLeftSon() != null);
+        return (node.getParent().getLeftSon() == node);
     }
     private boolean isRightSon(Node<T,K> node) {
-        return (node.getParent().getRightSon() != null);
+        return (node.getParent().getRightSon() == node);
     }
 
     private boolean hasRightSon(Node<T,K> node) {
@@ -29,6 +30,128 @@ public class BinaryTree<T, K extends Comparable<K>> {
 
     private boolean hasLeftSon(Node<T,K> node) {
         return (node.getLeftSon() != null);
+    }
+
+    public void inOrder() {
+        if (this.root == null){
+            return;
+        }
+        Stack<Node<T,K>> stack = new Stack<>();
+        Node<T,K> tempNode = this.root;
+        while(tempNode != null || stack.size() > 0){
+            while(tempNode != null){
+                stack.push(tempNode);
+                tempNode = tempNode.getLeftSon();
+            }
+            tempNode = stack.pop();
+            System.out.println(tempNode.getData());
+            tempNode = tempNode.getRightSon();
+        }
+    }
+
+    public void preOrder() {
+        if (this.root == null) {
+            return;
+        }
+        Stack<Node<T,K>> stack = new Stack<>();
+        Node<T,K> tempNode;
+        stack.push(this.root);
+
+        while(!stack.isEmpty()){
+            tempNode = stack.pop();
+            System.out.println(tempNode.getData());
+            if (tempNode.getLeftSon() != null) {
+                stack.push(tempNode.getLeftSon());
+            }
+            if (tempNode.getRightSon() != null) {
+                stack.push(tempNode.getRightSon());
+            }
+        }
+    }
+
+    public void postOrder(){
+        if (this.root == null){
+            return;
+        }
+        Stack<Node<T,K>> stack = new Stack<>();
+        Node<T,K> tempNode;
+        Node<T,K> head = this.root;
+        boolean finished;
+
+        stack.push(this.root);
+
+        while(!stack.isEmpty()) {
+            tempNode = stack.peek();
+            finished = (tempNode.getRightSon() == head ||
+                        tempNode.getLeftSon() == head);
+
+            if (this.isLeaf(tempNode) || finished) {
+                stack.pop();
+                System.out.println(tempNode.getData());
+                head = tempNode;
+
+            } else {
+                if (tempNode.getRightSon() != null) {
+                    stack.push(tempNode.getRightSon());
+                }
+                if (tempNode.getLeftSon() != null) {
+                    stack.push(tempNode.getLeftSon());
+                }
+            }
+        }
+    }
+
+    public void levelOrder() {
+        if (this.root == null){
+            return;
+        }
+        ArrayList<Node<T,K>> stack = new ArrayList<>();
+        stack.add(this.root);
+        System.out.println(this.root.getData());
+        do {
+            int tempSize = stack.size();
+            for (int i = 0; i < tempSize; i++) {
+                Node<T,K> tempNode = stack.remove(0);
+                if (tempNode.getLeftSon() != null) {
+                    stack.add(tempNode.getLeftSon());
+                    System.out.println(tempNode.getLeftSon().getData());
+                }
+                if (tempNode.getRightSon() != null) {
+                    stack.add(tempNode.getRightSon());
+                    System.out.println(tempNode.getRightSon().getData());
+                }
+            }
+        } while (!stack.isEmpty());
+    }
+    // TO DO
+    private K findIntervalStart(K minKey, K maxKey){
+        K intervalStart = maxKey;
+        Node<T,K> tempNode = this.root;
+        while(tempNode != null) {
+            if (!this.isLeaf(tempNode)) {
+                if (intervalStart.compareTo(tempNode.getKey()) > 0){
+                    intervalStart = tempNode.getKey();
+                }
+                if (minKey.compareTo(tempNode.getKey()) == 0){
+                    intervalStart = tempNode.getKey();
+                    break;
+                } else if (minKey.compareTo(tempNode.getKey()) > 0) {
+                    tempNode = tempNode.getRightSon();
+                } else {
+                    tempNode = tempNode.getLeftSon();
+                }
+            } else {
+                if (intervalStart.compareTo(tempNode.getKey()) > 0){
+                    System.out.println();
+                }
+            }
+
+        }
+        return intervalStart;
+    }
+
+    public void intervalSearch(K minKey, K maxKey){
+        // TO DO
     }
 
     public K minKey() {
@@ -97,22 +220,26 @@ public class BinaryTree<T, K extends Comparable<K>> {
         return tempNode;
     }
 
+    /**
+     * returns depth of binary tree
+     * @return depth of binary tree
+     */
     public int treeDepth() {
         int depth = 0;
         if (this.root == null){
             return depth;
         }
-        ArrayList<Node<T,K>> stack = new ArrayList<>();
-        stack.add(this.root);
+        Stack<Node<T,K>> stack = new Stack<>();
+        stack.push(this.root);
         do {
             int tempSize = stack.size();
             for (int i = 0; i < tempSize; i++) {
-                Node<T,K> tempNode = stack.remove(0);
+                Node<T,K> tempNode = stack.pop();
                 if (tempNode.getLeftSon() != null){
-                    stack.add(tempNode.getLeftSon());
+                    stack.push(tempNode.getLeftSon());
                 }
                 if (tempNode.getRightSon() != null){
-                    stack.add(tempNode.getRightSon());
+                    stack.push(tempNode.getRightSon());
                 }
             }
             depth ++;
@@ -121,6 +248,11 @@ public class BinaryTree<T, K extends Comparable<K>> {
         return depth;
     }
 
+    /**
+     * finds item in binary tree
+     * @param key - key of node
+     * @return value of node, null if node does not exists
+     */
     public T find(K key) {
         if (this.root == null) {
             return null;
@@ -138,6 +270,12 @@ public class BinaryTree<T, K extends Comparable<K>> {
         }
         return null;
     }
+
+    /**
+     * finds and returns node of binary tree
+     * @param key - node key
+     * @return node with corresponding key, null if node does not exist
+     */
     private Node<T,K> findNode(K key){
         if (this.root == null) {
             return null;
@@ -156,10 +294,16 @@ public class BinaryTree<T, K extends Comparable<K>> {
         return null;
     }
 
+    /**
+     * inserts new node to binary tree
+     * @param data - node data
+     * @param key - node key
+     * @return true if successful, otherwise false
+     */
     public boolean insert(T data, K key) {
+        // if tree is empty
         if (this.root == null) {
             this.root = new Node<>(data, key, null);
-            //this.root = new Node<>(data, key, null);
             return true;
         } else {
             Node<T, K> tempNode = this.root;
@@ -185,19 +329,88 @@ public class BinaryTree<T, K extends Comparable<K>> {
         return false;
     }
 
-    public Node<T,K> delete(K key) {
+    /**
+     * deletes node of binary tree
+     * @param key - key of node
+     * @return value of deleted node or null if tree is empty
+     */
+    public T delete(K key) {
         Node<T,K> nodeToDelete = this.findNode(key);
-        if (nodeToDelete == null){
+        if (nodeToDelete == null) {
             return null;
         }
-        if (this.isRoot(nodeToDelete) && this.isLeaf(nodeToDelete)){
+        // if tree has only one item, delete root and all its references
+        if (this.isRoot(nodeToDelete) && this.isLeaf(nodeToDelete)) {
+            T data = nodeToDelete.getData();
+            nodeToDelete.delete();
             this.root = null;
-            return nodeToDelete;
+            return data;
         }
+        // if node is leaf, just delete leaf and all its references
         if (this.isLeaf(nodeToDelete)) {
-            
-            return nodeToDelete;
+            if (this.isLeftSon(nodeToDelete)) {
+                nodeToDelete.getParent().setLeftSon(null);
+            } else {
+                nodeToDelete.getParent().setRightSon(null);
+            }
+            T data = nodeToDelete.getData();
+            nodeToDelete.delete();
+            nodeToDelete = null;
+            return data;
+        }
+        if (!this.isLeaf(nodeToDelete)) {
+            Node<T,K> successorNode = this.findNodeSuccessor(nodeToDelete);
+            T data = nodeToDelete.getData();
+            this.swapNodes(nodeToDelete, successorNode);
+            if (this.isRoot(nodeToDelete)) {
+
+                if (this.isLeftSon(successorNode)) {
+                    successorNode.getParent().setLeftSon(null);
+                } else {
+                    successorNode.getParent().setRightSon(null);
+                }
+
+            } else if (this.isLeftSon(successorNode)) {
+                successorNode.getParent().setLeftSon(null);
+            } else {
+                successorNode.getParent().setRightSon(null);
+            }
+            successorNode.delete();
+            successorNode = null;
+            return data;
         }
         return null;
+    }
+
+    /**
+     * finds inOrder or preOrder successor for node
+     * @param node - node for which successor is sought
+     * @return successor node
+     */
+    private Node<T,K> findNodeSuccessor(Node<T,K> node) {
+        Node<T,K> tempNode = node;
+        if (tempNode.getLeftSon() != null) {
+            tempNode = tempNode.getLeftSon();
+            while(tempNode.getRightSon() != null){
+                tempNode = tempNode.getRightSon();
+            }
+        } else {
+            tempNode = tempNode.getRightSon();
+            while(tempNode.getLeftSon() != null){
+                tempNode = tempNode.getLeftSon();
+            }
+        }
+        return tempNode;
+    }
+
+    private void swapNodes(@NotNull Node<T,K> node1, @NotNull Node<T,K> node2){
+        T tempData = node1.getData();
+        K tempKey = node1.getKey();
+
+        node1.setData(node2.getData());
+        node1.setKey(node2.getKey());
+
+        node2.setData(tempData);
+        node2.setKey(tempKey);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,49 @@ public class Main {
 
     public static void main(String[] args) {
         BinaryTree<Integer, Integer> tree = new BinaryTree<>();
-        boolean r = tree.insert(50, 50);
-        System.out.println(r);
-        r = tree.insert(20, 20);
-        System.out.println(r);
-        r = tree.insert(100, 100);
-        System.out.println(r);
-        r = tree.insert(10, 10);
-        System.out.println(r);
-        r = tree.insert(30, 30);
-        System.out.println(r);
-        r = tree.insert(80, 80);
-        System.out.println(r);
-        r = tree.insert(150, 150);
-        System.out.println(r);
-        r = tree.insert(40, 40);
-        System.out.println(r);
+//        boolean r = tree.insert(50, 50);
+//        System.out.println(r);
+//        r = tree.insert(20, 20);
+//        System.out.println(r);
+//        r = tree.insert(100, 100);
+//        System.out.println(r);
+//        r = tree.insert(10, 10);
+//        System.out.println(r);
+//        r = tree.insert(30, 30);
+//        System.out.println(r);
+//        r = tree.insert(80, 80);
+//        System.out.println(r);
+//        r = tree.insert(150, 150);
+//        System.out.println(r);
+//        r = tree.insert(40, 40);
+//        System.out.println(r);
+        tree.insert(8, 8);
+        tree.insert(3, 3);
+        tree.insert(10, 10);
+        tree.insert(1, 1);
+        tree.insert(6, 6);
+        tree.insert(14, 14);
+        tree.insert(4, 4);
+        tree.insert(7, 7);
+        tree.insert(13, 13);
+        tree.inOrder();
+        int i = tree.delete(3);
+        System.out.println("********************");
+        tree.inOrder();
+        i = tree.delete(8);
+        System.out.println("********************");
+        tree.inOrder();
+        i = tree.delete(6);
+        System.out.println("********************");
+        tree.inOrder();
 
-        System.out.println(tree.find(40));
-        System.out.println(tree.find(49));
+
+        tree.inOrder();
+        System.out.println("********************");
+        tree.levelOrder();
+        System.out.println("********************");
+        tree.preOrder();
+        System.out.println("********************");
+        tree.postOrder();
     }
 }
